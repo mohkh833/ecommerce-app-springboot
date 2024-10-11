128be12cecbd157eb91c65e5ac803808bb823225
Mohamed Khaled <55802148+mohkh833@users.noreply.github.com>
2024-10-11 17:23:27 +0300

    Update JWTUtils.java

--- src/main/java/com/Ecommerce_backend/utils/JWTUtils.java
+++ src/main/java/com/Ecommerce_backend/utils/JWTUtils.java
@@ -20,7 +20,7 @@ public class JWTUtils {
     private final SecretKey key;
 
     public JWTUtils() {
-        String secretString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+       
         byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
         this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
     }
